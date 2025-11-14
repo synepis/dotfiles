2bfd28cd89c1a794400f782bc2d7df2234ebcba7
Argo Synepis <frane.roje@gmail.com>
2025-11-14 15:45:15 +0100

    Delete everything to start fresh

--- i3/scripts/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-exec xautolock -time 5 \
-	-locker "i3lock -i ~/Downloads/wall1.png" \
-	-notify 30 \
-	-notifier "notify-send -u critical -t 1000 'LOCKING SCREEN in 30 seconds...'" \
-	-corners -000
-
